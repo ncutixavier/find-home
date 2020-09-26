79d3cca227af61e9e0a561c30918c782b98947e8
unknown <ncuti60@gmail.com>
2020-09-26 20:20:11 +0200

    update test

--- src/test/test.js
+++ src/test/test.js
@@ -1,9 +1,9 @@
 import chaiHttp from 'chai-http'
-import server from '../app'
 import chai from 'chai'
+import server from '../app'
 
 chai.use(chaiHttp)
-const expect = require('chai').expect;
+const expect = chai.expect;
 
 describe('FIND HOME API', () => {
     describe('/GET Welcome', () => {
@@ -25,6 +25,15 @@ describe('FIND HOME API', () => {
                     expect(res.statusCode).to.equal(200);
                     expect(res.body).to.be.an('object')
                     expect(res.body).to.be.an('object').with.property('users')
+                    done();
+                });
+        });
+
+        it('it should NOT GET all users', (done) => {
+            chai.request(server)
+                .get('/api/v1/user')
+                .end((err, res) => {
+                    expect(res.statusCode).to.equal(404);
                     done();
                 });
         });

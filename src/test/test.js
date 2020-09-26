13c5fee0d8197eea23e938aa192b516642269137
unknown <ncuti60@gmail.com>
2020-09-26 15:48:02 +0200

    test all users

--- src/test/test.js
+++ src/test/test.js
@@ -1,5 +1,5 @@
-import chaiHttp from'chai-http'
-import server from'../app'
+import chaiHttp from 'chai-http'
+import server from '../app'
 import chai from 'chai'
 
 
@@ -9,13 +9,27 @@ chai.use(chaiHttp)
 describe('FIND HOME API', () => {
     /*
       * Test the /GET route
-      */
+    */
     describe('/GET Welcome', () => {
         it('it should GET welcome', (done) => {
             chai.request(server)
                 .get('/')
                 .end((err, res) => {
                     res.should.have.status(200);
+                    done();
+                });
+        });
+    });
+    /*
+      * Test the /GET all users
+    */
+    describe('/GET Users', () => {
+        it('it should GET all users', (done) => {
+            chai.request(server)
+                .get('/api/v1/users')
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.users.should.be.a('array');
                     done();
                 });
         });

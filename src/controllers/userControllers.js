3c10ceb2d34becbdaa30988cba5b1be94b6ecd10
unknown <ncuti60@gmail.com>
2020-10-08 12:36:58 +0200

    ft:add package

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -2,10 +2,16 @@ import Model from '../database/models/'
 
 //create new user
 exports.createUser = async(req,res)=>{
-    const user = await Model.User.create(req.body)
+    try {
+        const user = await Model.User.create(req.body)
     res.status(200).json({
         user
     })
+    } catch (error) {
+        res.status(500).json({
+            message: res.__("can't post user")
+        })
+    }
 }
 
 //get all users
